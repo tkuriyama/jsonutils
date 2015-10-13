d4fdc0d88d9031fd6dd0f3718dd1b5a9c79fd2b0
THK <taro.kuriyama@gmail.com>
2015-10-13 18:33:53 -0400

    Adding test folder and setup script

---
+++ Python/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name = "JSON Tools",
+    version = "0.1",
+    packages = find_packages(),
+    scripts = [],
+
+    install_requires = [],
+    package_data = {},
+
+    author = 'Taro Kuriyama',
+    author_email = 'taro@tarokuriyama.com',
+    description = '',
+    license = 'MIT'
+)
