dcfb42448007261f13157d8bc016f9b685212a22
Taro Kuriyama <taro@tarokuriyama.com>
2015-11-14 16:21:10 -0500

    Updating node printing in logger and tests

--- Python/pyjson_utils/logger.py
+++ Python/pyjson_utils/logger.py
@@ -12,7 +12,7 @@ def trim(n):
 def parse_config(config):
     """Parse config dictionary, return default values if necessary."""
     sep = config['sep'] if 'sep' in config else ': '
-    indent = config['indent'] if 'indent' in config else '----'
+    indent = config['indent'] if 'indent' in config else ' -- '
     trim_key = trim(config['trim_key']) if 'trim_key' in config else trim(20)
     trim_val = trim(config['trim_val']) if 'trim_val' in config else trim(20)
     return sep, indent, trim_key, trim_val
@@ -21,9 +21,18 @@ def is_iter(arg):
     """Return True if argument is iterable."""
     return isinstance(arg, list) or isinstance(arg, tuple)
 
-def format_text(text, indent, depth):
-    """"""
-    return (indent + ' ') * (depth - 1) + ' ' + text
+def format_node(text, indent, depth):
+    """
+    Args
+        text: string of node to print
+        indent: string of tree indentation chars
+        depth: int of tree depth, 0 = root
+    Returns
+        Formatted string.
+    """
+    space = ' ' * ((len(indent) + 1) * (depth - 2))
+    sep = '|' + indent if depth > 1 else ''
+    return space + sep + text
 
 def dict_to_tree(d, mapping={}, config={}):
     """
@@ -42,14 +51,14 @@ def dict_to_tree(d, mapping={}, config={}):
     print_stack = [(key, d[key], 0) for key, val in d.items()]
     while print_stack:
         key, val, depth = print_stack.pop()
-        print_key = sep.join(key) if is_iter(key) else key
-        output.append((format_text(print_key, indent, depth)))
+        print_key = sep.join(key) if is_iter(key) else str(key)
+        output.append((format_node(print_key, indent, depth)))
 
         if isinstance(val, dict):
             next_keys = [(k, val[d], depth + 1) for k, v in val.items()]
             print_stack.extend(next_keys)
         else:
-            print_val = sep.join(val) if is_iter(val) else key
-            output.append((format_text(print_val, indent, depth + 1)))
+            print_val = sep.join(val) if is_iter(val) else str(val)
+            output.append((format_node(print_val, indent, depth + 1)))
 
     return '\n'.join(output)
--- Python/pyjson_utils/test/test_logger.py
+++ Python/pyjson_utils/test/test_logger.py
@@ -21,6 +21,19 @@ class TestDictToTreeHelpers:
         test = 'oahdubowbfocbqeocq18boqbsf8g19oubwdoqbs9c91brqsdc'
         test_trim = logger.trim(20)
         assert sep == ': '
-        assert indent == '--  '
+        assert indent == ' -- '
         assert trim_key(test) == test_trim(test)
         assert trim_val(test) == test_trim(test)
+
+    def test_is_iter(self):
+        """Test iterable type tester."""
+        f = logger.is_iter
+        assert f([1, 2, 3]) is True
+        assert f((1, 2, 3)) is True
+        assert f({'a': 1}) is False
+
+    def test_format_node(self):
+        """Test node to string function."""
+        f = logger.format_node
+        assert f('a', '----', 1) == 'a'
+        assert f('a', '----', 2) == '|----a'
