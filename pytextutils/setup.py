9e827a9440c785b5547fa73e26f7e168e1530085
THK <taro.kuriyama@gmail.com>
2016-01-01 11:25:36 -0500

    Updating setup for new naming

---
+++ pytextutils/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name = "Python Text Utilities",
+    version = "0.1.3",
+    packages = find_packages('pytextutils'),
+    scripts = [],
+
+    install_requires = [],
+    package_data = {},
+
+    author = 'Taro Kuriyama',
+    author_email = 'taro@tarokuriyama.com',
+    description = '',
+    license = 'MIT'
+)
